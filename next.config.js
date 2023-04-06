798dc4b28aca23fc8ca8e47b6315b609463503ea
Kazufumi Tsuboi <92964713+Kazufumi-Tsuboi@users.noreply.github.com>
2023-04-06 16:40:24 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export"
+  trailingSlash: true,
+  output: "export",
+  exportPathMap: function () {
+    return { 
+        '/': {page: '/'},
+        '/zeng': {page: '/zeng/zeng'},
+        '/_app': {page: '/_app/_app'},
+        '/_document': {page: '/_document/_document'},
+        '/contact': {page: '/contact/contact'},
+        '/index': {page: '/index/index'},
+        '/itoh': {page: '/itoh/itoh'},
+        '/labs': {page: '/labs/labs'},
+        '/seminars': {page: '/seminars/seminars'},
+        '/workshops': {page: '/workshops/workshops'},
+    };
+},
 }
 
 module.exports = nextConfig
