df7796e5e07c52073414ed88dfcd6ba068cb4bf8
letsgetoutofthistown <thewindblewmyhatoff@gmail.com>
2023-07-03 16:16:28 +0900

    remove unnecessary configs

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   output: "export",
-  exportPathMap: function () {
-    return { 
-        '/': {page: '/'},
-        '/zeng': {page: '/zeng'},
-        '/_app': {page: '/_app'},
-        '/_document': {page: '/_document'},
-        '/contact': {page: '/contact'},
-        '/index': {page: '/index'},
-        '/itoh': {page: '/itoh'},
-        '/labs': {page: '/labs'},
-        '/seminars': {page: '/seminars'},
-        '/workshops': {page: '/workshops'},
-    };
-},
 }
 
 module.exports = nextConfig
