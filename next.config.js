adac8103e332267a0159e91924a9b5d212661e56
letsgetoutofthistown <thewindblewmyhatoff@gmail.com>
2023-07-03 16:13:31 +0900

    recover config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "out",
   reactStrictMode: true,
+  trailingSlash: true,
   output: "export",
+  exportPathMap: function () {
+    return { 
+        '/': {page: '/'},
+        '/zeng': {page: '/zeng'},
+        '/_app': {page: '/_app'},
+        '/_document': {page: '/_document'},
+        '/contact': {page: '/contact'},
+        '/index': {page: '/index'},
+        '/itoh': {page: '/itoh'},
+        '/labs': {page: '/labs'},
+        '/seminars': {page: '/seminars'},
+        '/workshops': {page: '/workshops'},
+    };
+},
 }
 
 module.exports = nextConfig
