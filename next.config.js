555557f64a181b7205a57169c27e36ad2840be79
letsgetoutofthistown <thewindblewmyhatoff@gmail.com>
2023-07-03 14:31:03 +0900

    edit npm run build

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "out",
   reactStrictMode: true,
-  trailingSlash: true,
   output: "export",
-  exportPathMap: function () {
-    return { 
-        '/': {page: '/'},
-        '/zeng': {page: '/zeng'},
-        '/_app': {page: '/_app'},
-        '/_document': {page: '/_document'},
-        '/contact': {page: '/contact'},
-        '/itoh': {page: '/itoh'},
-        '/labs': {page: '/labs'},
-        '/seminars': {page: '/seminars'},
-        '/workshops': {page: '/workshops'},
-    };
-},
 }
 
 module.exports = nextConfig
